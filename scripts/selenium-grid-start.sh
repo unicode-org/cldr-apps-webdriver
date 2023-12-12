c9f37688e8ad7affbb549b9ea65a049e8646a82f
Tom Bishop <thomasbishop@wenlin.com>
2023-12-12 17:37:24 -0500

    CLDR-17248 Measure vote timing; update cldr-apps-webdriver (#28)
    -Update selenium-grid-start.sh to work with current Selenium
    
    -Update instructional comments in SurveyDriver.java

--- scripts/selenium-grid-start.sh
+++ scripts/selenium-grid-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-standalone-3.141.59.jar -role hub &
+HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-4.16.1.jar standalone &
 
 for ((PORT = 5555; PORT <= 5564; PORT++))
 do  
-	HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-standalone-3.141.59.jar -role node -port $PORT &
+	HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-4.16.1.jar standalone --port $PORT &
 done
