37efbde91d4ae4ddc018dbf097e6805543e8f9ef
Tom Bishop <thomasbishop@wenlin.com>
2023-12-27 10:19:02 -0800

    CLDR-17248 Change cldr-apps-webdriver system for simulated users (#31)
    -New SurveyDriverCredentials.java
    
    -Simulated users have pseudo email addresses like driver-123@cldr-apps-webdriver.org
    
    -See related PR 3424 for cldr
    
    -Remove cldr-add-webdrivers.sql, no longer needed
    
    -Store new CLDR_WEBDRIVER_PASSWORD in new file surveydriver.properties, not in git, see .gitignore
    
    -Avoid dependency on old survey?email... api, simulate click on Log In button instead
    
    -New methods loginWithButton, clickButtonByXpath, inputTextByXpath, getClickableElementByXpath
    
    -Update selenium-java from 4.3.0 to 4.16.1
    
    -Simplify selenium-grid-start.sh; only one call is needed with current selenium; add --selenium-manager true
    
    -Update apache.logging.log4j from 2.17.2 to 2.20.0
    
    -Remove gson dependency, no longer needed
    
    -Minor refactoring
    
    -Comments

--- scripts/selenium-grid-start.sh
+++ scripts/selenium-grid-start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-4.16.1.jar standalone &
-
-for ((PORT = 5555; PORT <= 5563; PORT++))
-do  
-	HUB_URL=http://localhost:4444/grid/register java -jar selenium-server-4.16.1.jar standalone --port $PORT &
-done
+java -jar selenium-server-4.16.1.jar standalone --selenium-manager true &
